aa5557ebe4064f165e16192423604cfd614cee82
Tong Tang <pichip22@hotmail.com>
2023-09-10 14:45:28 -0400

    completed

--- server/app.py
+++ server/app.py
@@ -6,7 +6,7 @@ from flask_migrate import Migrate
 from models import db, Zookeeper, Enclosure, Animal
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////home/yealt/Development/code/phase-4/python-p4-flask-sqlalchemy-lab/server/instance/app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 migrate = Migrate(app, db)
@@ -19,15 +19,79 @@ def home():
 
 @app.route('/animal/<int:id>')
 def animal_by_id(id):
-    return ''
+    animal = Animal.query.filter(Animal.id == id).first()
+
+    if not animal:
+        response_body = '<h1>Animal not found</h1>'
+        response = make_response(response_body, 404)
+        return response
+    
+    response_body = f'''
+        <h1>Animal</h1>
+        <ul>ID: {animal.id}</ul>
+        <ul>Name: {animal.name}</ul>
+        <ul>Species: {animal.species}</ul>
+        <ul>Zookeeper: {animal.zookeeper.name}</ul>
+        <ul>Enclosure: {animal.enclosure.environment}</ul>
+    '''
+    response = make_response(response_body, 200)
+    return response
+
 
 @app.route('/zookeeper/<int:id>')
 def zookeeper_by_id(id):
-    return ''
+    zookeeper = Zookeeper.query.filter(Zookeeper.id == id).first()
+
+    if not zookeeper:
+        response_body = '<h1>Zookeeper not found</h1>'
+        response = make_response(response_body, 404)
+        return response
+    
+    response_body = f'''
+        <h1>Zookeeper</h1>
+        <ul>ID: {zookeeper.id}</ul>
+        <ul>Name: {zookeeper.name}</ul>
+        <ul>Birthday: {zookeeper.birthday}</ul>
+    '''
+
+    animals = [animal for animal in zookeeper.animals]
+
+    if animals:
+        for animal in animals:
+            response_body += f'<ul>Animal: {animal.name}</ul>'
+    else:
+        response_body += '<h1>No animals</h1>'
+
+    response = make_response(response_body, 200)
+
+    return response
 
 @app.route('/enclosure/<int:id>')
 def enclosure_by_id(id):
-    return ''
+    environment = Enclosure.query.filter(Enclosure.id == id).first()
+
+    if not environment:
+        response_body = '<h1>Enclosure not found</h1>'
+        response = make_response(response_body, 404)
+        return response
+    
+    response_body = f'''
+        <h1>Enclosure</h1>
+        <ul>ID: {environment.id}</ul>
+        <ul>Environment: {environment.environment}</ul>
+        <ul>Open to Visitors: {environment.open_to_visitors}</ul>
+    '''
+
+    animals = [animal for animal in environment.animals]
+
+    if animals:
+        for animal in animals:
+            response_body += f'<ul>Animal: {animal.name}</ul>'
+    else:
+        response_body += '<h1>No animals</h1>'
+    
+    response = make_response(response_body, 200)
+    return response
 
 
 if __name__ == '__main__':
--- server/models.py
+++ server/models.py
@@ -11,13 +11,35 @@ class Zookeeper(db.Model):
     __tablename__ = 'zookeepers'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    birthday = db.Column(db.String)
 
+    animals = db.relationship('Animal', backref='zookeeper')
+
+    def __repr__(self):
+        return f'<Zookeeper {self.name}, {self.birthday}>'
+    
 class Enclosure(db.Model):
     __tablename__ = 'enclosures'
 
     id = db.Column(db.Integer, primary_key=True)
+    environment = db.Column(db.String)
+    open_to_visitors = db.Column(db.Boolean)
 
+    animals = db.relationship('Animal', backref='enclosure')
+
+    def __repr__(self):
+        return f'<Enclosure {self.environment}, {self.open_to_visitors}>'
+    
 class Animal(db.Model):
     __tablename__ = 'animals'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    species = db.Column(db.String)
+    
+    zookeeper_id = db.Column(db.Integer, db.ForeignKey('zookeepers.id'))
+    enclosure_id = db.Column(db.Integer, db.ForeignKey('enclosures.id'))
+
+    def __repr__(self):
+        return f'<Animal {self.name}, {self.species}>'    
